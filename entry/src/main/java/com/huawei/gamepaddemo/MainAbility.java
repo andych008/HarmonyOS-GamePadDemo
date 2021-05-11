b200e173c2015c5f2974424283d8667977b06f97
Tran Thanh Huy 00564191 <tran.thanh.huy@huawei.com>
2021-05-11 15:41:48 +0900

    Init project

---
+++ entry/src/main/java/com/huawei/gamepaddemo/MainAbility.java
@@ -0,0 +1,14 @@
+package com.huawei.gamepaddemo;
+
+import com.huawei.gamepaddemo.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+
+}
